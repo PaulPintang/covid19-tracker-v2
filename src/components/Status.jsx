e91b802e06ec31b44f80588ae9f3ac25d4bcb03e
puldyastin <puldyastin@gmail.com>
2021-12-21 09:24:38 +0800

    table UI

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -24,48 +24,48 @@ const Status = (props) => {
         </a>
       </div> */}
       <div className="overflow-y-auto example">
-        <table className="min-w-full divide-y divide-gray-200 border-collapse w-full">
+        <table className="min-w-full divide-y dark:divide-gray-800 border-collapse w-full border-none">
           <thead className="sticky top-0 ">
             <tr>
               <th
                 scope="col"
-                className="md:px-6 lg:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm rounded-md"
+                className="md:px-3 lg:px-3 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-50 uppercase tracking-wider bg-white dark:bg-gray-700 dark:bg-opacity-60 shadow-sm rounded-tl-md"
               >
                 Countries
               </th>
               <th
                 scope="col"
-                className="hidden md:table-cell lg:table-cell py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm"
+                className="hidden md:table-cell lg:table-cell py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-50 uppercase tracking-wider bg-white dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
               >
                 Total Cases
               </th>
               <th
                 scope="col"
-                className="hidden md:table-cell lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm"
+                className="hidden md:table-cell lg:table-cell px-3 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-50 uppercase tracking-wider bg-white dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
               >
                 Active Cases
               </th>
               <th
                 scope="col"
-                className=" hidden md:table-cell lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm"
+                className=" hidden md:table-cell lg:table-cell px-3 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-50 uppercase tracking-wider bg-white dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
               >
                 Recovered
               </th>
               <th
                 scope="col"
-                className=" hidden md:table-cell lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm"
+                className=" hidden md:table-cell lg:table-cell px-3 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-50 uppercase tracking-wider bg-white dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
               >
                 Today Recovered
               </th>
               <th
                 scope="col"
-                className=" hidden md:table-cell lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm"
+                className=" hidden md:table-cell lg:table-cell px-3 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-50 uppercase tracking-wider bg-white dark:bg-gray-700 dark:bg-opacity-60 shadow-sm"
               >
                 Deaths
               </th>
               <th
                 scope="col"
-                className="rounded-md hidden md:table-cell lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm"
+                className="hidden md:table-cell lg:table-cell px-3 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-50 uppercase tracking-wider bg-white dark:bg-gray-700 dark:bg-opacity-60 shadow-sm rounded-tr-md"
               >
                 Today Deaths
               </th>
@@ -76,27 +76,39 @@ const Status = (props) => {
               .sort((a, b) => b.cases - a.cases)
               .slice(0, 5)
               .map((country) => (
-                <tr>
-                  <td class="py-2 pl-6">
+                <tr className="border-none">
+                  <td className="py-2 pl-3  border-none">
                     <div className="flex items-center gap-3">
                       <div class="w-7">
                         <img
                           src={country.countryInfo.flag}
                           alt=""
-                          class="w-full rounded-md"
+                          className="w-full rounded-md"
                         />
                       </div>
-                      <p className="font-medium uppercase text-sm">
+                      <p className="font-medium uppercase text-sm dark:text-gray-300">
                         {country.country}
                       </p>
                     </div>
                   </td>
-                  <td>{country.cases}</td>
-                  <td>{country.active}</td>
-                  <td>{country.recovered}</td>
-                  <td>{country.todayRecovered}</td>
-                  <td>{country.deaths}</td>
-                  <td>{country.todayDeaths}</td>
+                  <td className="dark:text-gray-400 py-2 pl-3">
+                    {country.cases}
+                  </td>
+                  <td className="dark:text-gray-400 py-2 pl-3">
+                    {country.active}
+                  </td>
+                  <td className="dark:text-gray-400 py-2 pl-3">
+                    {country.recovered}
+                  </td>
+                  <td className="dark:text-gray-400 py-2 pl-3">
+                    {country.todayRecovered}
+                  </td>
+                  <td className="dark:text-gray-400 py-2 pl-3">
+                    {country.deaths}
+                  </td>
+                  <td className="dark:text-gray-400 py-2 pl-3">
+                    {country.todayDeaths}
+                  </td>
                 </tr>
               ))}
           </tbody>
