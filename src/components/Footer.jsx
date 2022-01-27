a977c4de739bc63b8fa713eea31eefd74296c6fc
puldyastin <puldyastin@gmail.com>
2022-01-27 21:21:33 +0800

    add developer external link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,18 @@ const Footer = () => {
   return (
     <div class="flex justify-center h-48 bg-white dark:bg-gray-800">
       <div className="flex items-center h-full">
-        <p className="text-xl dark:text-gray-300 italic shadow-inner">
-          THIS COVID-19 TRACKER WEBSITE IS UNDER DEVELOPMENT
-        </p>
+        <div className="flex flex-col items-center">
+          <p className="text-xl dark:text-gray-300 italic shadow-inner">
+            THIS COVID-19 TRACKER WEBSITE IS UNDER DEVELOPMENT
+          </p>
+          <a
+            href="https://impaul.netlfify.app"
+            target="blank"
+            className="italic dark:text-gray-200 text-sm dark:hover:text-gray-400 transition-all"
+          >
+            Developer ?
+          </a>
+        </div>
       </div>
     </div>
   );
