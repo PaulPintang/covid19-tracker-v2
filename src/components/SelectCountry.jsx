7abdec4607fc11068fd7dd2694f07422912efff2
puldyastin <puldyastin@gmail.com>
2022-01-31 22:01:43 +0800

    select UI

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -12,19 +12,51 @@ const SelectCountry = () => {
             placeholder="Search for a Country"
           />
         </div>
-        <div>
-          {/* <select
-            className="px-4 text-gray-600 py-3 w-full rounded-md text-sm focus:outline-none dark:bg-gray-800 dark:text-gray-100 dark:focus:border-gray-800 dark:border-opacity-20 focus:border-opacity-60 border border-gray-200 border-opacity-40 shadow-sm bg-white"
-            name=""
-            id=""
-          >
-            <option className="bg-white shadow-sm" value="">
+        <div class="relative inline-block text-left">
+          <div>
+            <button
+              type="button"
+              class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none border-opacity-30"
+              id="menu-button"
+              aria-expanded="true"
+              aria-haspopup="true"
+            >
               Filter by Region
-            </option>
-            <option value=""></option>
-            <option value=""></option>
-            <option value=""></option>
-          </select> */}
+              <svg
+                class="-mr-1 ml-2 h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div
+            class="hidden origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="menu-button"
+            tabindex="-1"
+          >
+            <div class="py-1" role="none">
+              <a
+                href="#"
+                class="text-gray-700 block px-4 py-2 text-sm"
+                role="menuitem"
+                tabindex="-1"
+                id="menu-item-0"
+              >
+                Account settings
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex w-full justify-between">
