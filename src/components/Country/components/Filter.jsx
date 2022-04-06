30b27913582af7752c27289c4a1822b689ead2aa
PaulPintang <pauljpintang@gmail.com>
2022-04-06 14:48:54 +0800

    fixed: dropdown layout on mobile

--- src/components/Country/components/Filter.jsx
+++ src/components/Country/components/Filter.jsx
@@ -54,70 +54,68 @@ const Filter = ({
       <h1 className="text-2xl dark:text-white pb-7 py-10">Countries</h1>
       <div className="flex justify-between flex-wrap-reverse md:flex-no-wrap lg:flex-nowrap gap-4">
         <div className="flex gap-3 w-full md:w-auto lg:w-auto justify-between">
-          <div>
-            <div className="relative inline-block text-left w-full">
-              <div>
-                <button
-                  onClick={handleClickCountry}
-                  type="button"
-                  className="flex justify-between md:w-52 lg:w-52 w-full  rounded-md border border-gray-300 px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none border-opacity-30 dark:bg-gray-800 dark:text-gray-400"
-                  id="menu-button"
-                  aria-expanded="true"
-                  aria-haspopup="true"
+          <div className="relative inline-block text-left w-full">
+            <div>
+              <button
+                onClick={handleClickCountry}
+                type="button"
+                className="flex justify-between md:w-48 lg:w-48 w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none border-opacity-30 dark:bg-gray-800 dark:text-gray-400"
+                id="menu-button"
+                aria-expanded="true"
+                aria-haspopup="true"
+              >
+                {selectedCountry}
+                <svg
+                  className="-mr-1 ml-2 h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
                 >
-                  {selectedCountry}
-                  <svg
-                    className="-mr-1 ml-2 h-5 w-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
-              {/* Popup selection */}
-              {showCountry ? (
-                <div
-                  className="transition-all h-56 overflow-auto origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg bg-white dark:bg-gray-800 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="menu-button"
-                  tabIndex="-1"
-                >
-                  {showCountry && (
-                    <div className="py-1" role="none">
-                      {countries
-                        .filter(
-                          (region) => region.continent === selectedContinent
-                        )
-                        .sort()
-                        .map((item, i) => (
-                          <button
-                            onClick={handleSelectCountry}
-                            href="#"
-                            className="w-full text-left text-gray-700  px-4 py-2 text-sm dark:bg-gray-800 dark:text-gray-400 hover:dark:bg-gray-600 transition-all hover:bg-gray-200"
-                            role="menuitem"
-                            tabIndex="-1"
-                            id="menu-item-0"
-                            key={i}
-                            value={item.country}
-                          >
-                            {item.country}
-                          </button>
-                        ))}
-                    </div>
-                  )}
-                </div>
-              ) : (
-                ""
-              )}
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
             </div>
+            {/* Popup selection */}
+            {showCountry ? (
+              <div
+                className="transition-all h-56 w-full overflow-auto origin-top-right absolute right-0 mt-2 rounded-md shadow-lg bg-white dark:bg-gray-800 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex="-1"
+              >
+                {showCountry && (
+                  <div className="py-1" role="none">
+                    {countries
+                      .filter(
+                        (region) => region.continent === selectedContinent
+                      )
+                      .sort()
+                      .map((item, i) => (
+                        <button
+                          onClick={handleSelectCountry}
+                          href="#"
+                          className="w-full text-left text-gray-700 px-4 py-2 text-sm dark:bg-gray-800 dark:text-gray-400 hover:dark:bg-gray-600 transition-all hover:bg-gray-200"
+                          role="menuitem"
+                          tabIndex="-1"
+                          id="menu-item-0"
+                          key={i}
+                          value={item.country}
+                        >
+                          {item.country}
+                        </button>
+                      ))}
+                  </div>
+                )}
+              </div>
+            ) : (
+              ""
+            )}
           </div>
           <div className="relative inline-block text-left w-full">
             <div>
